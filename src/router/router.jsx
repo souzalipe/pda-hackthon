646258ec208e2321d68ae5ef863b27af094751c2
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-11-24 13:41:07 -0300

    feat: added hotel details page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { DashboardPage } from "../pages/Dashboard/DashboardPage";
+import { HotelDetails } from "../pages/HotelDetails";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <DashboardPage />
+    element: <DashboardPage />,
+  },
+  {
+    path: "/hotel/:id",
+    element: <HotelDetails />,
   },
 ]);
